c953785ac8797dc6c600cb5effe89fda7f3c9400
cakkypamucm <119052172+cakkypamucm@users.noreply.github.com>
2023-06-18 19:11:41 +0300

    git init

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    extends: ["stylelint-config-cakkypamucm", "stylelint-config-standard-vue/scss", "stylelint-config-prettier-scss"],
+    rules: {
+        "scss/load-no-partial-leading-underscore": null
+    }
+};
