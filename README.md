c92602f48e8cbb9e2d80d5649e83fe0f508396e3
cakkypamucm <119052172+cakkypamucm@users.noreply.github.com>
2023-06-18 19:11:41 +0300

    git init

---
+++ README.md
@@ -0,0 +1,7 @@
+This is `stylelint` config based on [cakkypamucm/stylelint-config](https://github.com/cakkypamucm/stylelint-config) with `vue` support
+
+### Install
+
+```shell
+npm i github:cakkypamucm/stylelint-config-vue --save-dev
+```
---
+++ index.js
@@ -0,0 +1,11 @@
+/* eslint sort-keys/sort-keys-fix: "error" */
+/* eslint-disable sort-keys/sort-keys-fix */
+
+module.exports = {
+    extends: ["stylelint-config-cakkypamucm", "stylelint-config-standard-vue/scss", "stylelint-config-prettier-scss"],
+    /* eslint-enable sort-keys/sort-keys-fix */
+    rules: {
+        "scss/load-no-partial-leading-underscore": null
+    }
+    /* eslint-disable sort-keys/sort-keys-fix */
+};
